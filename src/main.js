272bf429c5644c56d1b9db19f83401652118ff57
Sergio Gonzalez <89554725+sergiogonzalezyes@users.noreply.github.com>
2023-12-28 20:06:18 -0600

    checking out different scaffolding tool

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import { pinia } from './store';
-import router from './router';
-
-createApp(App).use(router).use(pinia).mount('#app');
